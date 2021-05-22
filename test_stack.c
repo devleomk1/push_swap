4f73050a5519baeb70dd160f778ef85af1d6daa0
devleo <wltnskjs@naver.com>
2021-05-22 23:33:04 +0900

    Update. test_stack.c

--- test_stack.c
+++ test_stack.c
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 12:31:59 by jisokang          #+#    #+#             */
-/*   Updated: 2021/05/21 16:30:33 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/05/22 03:10:56 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ typedef struct		s_list
 {
 	void			*content;
 	struct s_list	*next;
-	struct s_list	*pre;
+	struct s_list	*prev;
 }					t_list;
 
 t_list	*ft_lstnew(void *content)
@@ -31,16 +31,20 @@ t_list	*ft_lstnew(void *content)
 		return (NULL);
 	new->content = content;
 	new->next = NULL;
-	new->pre = NULL;
+	new->prev = NULL;
 	return (new);
 }
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	t_list	*tmp;
+
 	if (lst == NULL || new == NULL)
 		return ;
-	new->next = *lst;
-	new->pre = NULL;
+	tmp = *lst;
+	new->prev = NULL;
+	new->next = tmp;
+	tmp->prev = new;
 	*lst = new;
 }
 
@@ -75,7 +79,7 @@ int	main(int argc, char **argv)
 	printf("Stack A\n");
 	while (curr != NULL)
 	{
-		printf("|%3s|\n", curr->content);
+		printf("|%s|>", curr->content);
 		curr = curr->next;
 	}
 	printf("|---|\n\n");
